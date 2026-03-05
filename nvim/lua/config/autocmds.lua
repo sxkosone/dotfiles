896ae1686265c43da94a6d97a71199ae5a3d97dd
Susanna Kosonen <susannakosonen@susanna-kosonen.local>
2026-03-05 10:11:18 -0500

    Hidelint and gym vim commands

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_user_command("Hidelint", function()
+  vim.diagnostic.disable(0)
+end, {})
+
+vim.api.nvim_create_user_command("Showlint", function()
+  vim.diagnostic.enable(0)
+end, {})
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,30 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
 -- === move to sidenav filenav on currently open file ===
 vim.keymap.set("n", "<leader>e", ":Neotree reveal<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>gy", function()
+  local file = vim.fn.expand("%:p")
+  local line = vim.fn.line(".")
+
+  -- Get repo root
+  local root =
+    vim.fn.systemlist("git -C " .. vim.fn.shellescape(vim.fn.expand("%:p:h")) .. " rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Not in a git repo", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Relative path from repo root
+  local rel_path = file:sub(#root + 2)
+
+  -- Get current branch
+  local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+
+  -- Get remote URL and convert to HTTPS
+  local remote = vim.fn.systemlist("git remote get-url origin")[1]
+  local github_url = remote:gsub("git@github%.com:", "https://github.com/"):gsub("%.git$", "")
+
+  local url = github_url .. "/blob/" .. branch .. "/" .. rel_path .. "#L" .. line
+  vim.fn.setreg("+", url)
+  vim.notify("Copied: " .. url)
+end, { desc = "Copy GitHub URL for current line" })
